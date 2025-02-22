4d8081a230f6427053d80f63c4f6a5d590a92523
kanata203 <kanaponpon789@gmail.com>
2025-02-22 16:04:22 +0900

    2025/2/22
    enemyの角度の調整

--- Assets/Scripts/enemymove.cs
+++ Assets/Scripts/enemymove.cs
@@ -3,25 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using System;
+using UnityEngine.Timeline;
 
 public class enemymove : MonoBehaviour
 {
     Ray ray;
    RaycastHit hit;
-    Vector3 direction;
-    public float distance = 10;
+   
+    public float distance = 1;
     public int raycount = 6;
     public float enemyFOV = 180;
-    public float wall_distance;
+    
     public Vector3 player_pos;
     public Vector3 wall_pos;
-    float enemy_foward = 30;
+   
     Rigidbody rb;
     Vector3 enemy_advance;
     public float enemy_speed;
+    float time;
     // Start is called before the first frame update
     void Start()
     {
+        time = 0;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -33,22 +36,23 @@ public class enemymove : MonoBehaviour
      */
     void Update()
     {
+        time += Time.deltaTime;
         enemy_advance = transform.forward;
-        rb.velocity = new Vector3(enemy_advance.x * enemy_speed, rb.velocity.y, enemy_advance.z * enemy_speed);
-        enemyFOV %= 360;
+        rb.velocity = enemy_advance.normalized * enemy_speed;
+        float minwalldist = 1000f;
         Vector3 vector = transform.position;//npcÇÃç¿ïW
         float anglestep = enemyFOV / raycount;
        
         bool hitplayer = false;
-        float start_angle = 90 - enemyFOV / 2;
+        float start_angle = transform.eulerAngles.y - enemyFOV / 2;
         for (int i = 0; i < raycount+1; i++) 
         {
             //float angle = anglestep * i + start_angle;
-            float angle = i * (360 / raycount);
+            float angle = start_angle - i * anglestep;
             Vector3 dic = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad),0f,Mathf.Sin(angle * Mathf.Deg2Rad));
             ray = new Ray(vector, dic);
             Debug.DrawRay(vector, ray.direction * distance, Color.red);
-            RaycastHit hit;
+            
             if(Physics.Raycast(ray,out hit,distance))//ÉåÉCÇ™âΩÇ©Ç…ìñÇΩÇ¡ÇΩÇÁ
             {
                
@@ -57,10 +61,16 @@ public class enemymove : MonoBehaviour
                     player_pos = hit.collider.transform.position;
                     hitplayer = true;
                 }
-                if(hit.collider.tag == "wall" && !hitplayer)
+                if(hit.collider.tag == "wall")
                 {
-                    wall_pos = hit.collider.transform.position;
-                   
+                    float dist = Vector3.Distance(hit.point, transform.position);
+                    if(dist < minwalldist)
+                    {
+                        minwalldist = dist;
+                        wall_pos = hit.point;
+
+                    }
+
                     Debug.Log(wall_pos);
                 }
                 
@@ -68,19 +78,26 @@ public class enemymove : MonoBehaviour
             }
             
         }
-        //ê≥ñ ÇÃï«Ç∆ÇÃãóó£Çë™ÇÈ
-        Vector3 walldic = new Vector3(0, 0, 1);//90ìxÇÃå¸Ç´
-        float dist = Vector3.Distance(wall_pos , transform.position);
-        Debug.Log(dist);
-       if (dist < distance)
+        
+        
+       float walldist = Vector3.Distance(wall_pos, transform.position);
+        Debug.Log(walldist);
+       if (walldist < 2.5f)
         {
             
-            direction = (wall_pos - transform.position).normalized;
-            float angle = Vector3.Angle(enemy_advance, direction);
-            if(angle <= enemy_foward)
+            Vector3 walldir = (wall_pos - transform.position).normalized;
+            float angle = Vector3.Angle(transform.forward, walldir);
+            if(angle >= -40 || angle <= 40)
             {
-                transform.Rotate(new Vector3(0, +9, 0));
-                Debug.Log("tomare");
+                if(time >= 1f)
+                {
+                    float rota;
+                    do {rota = UnityEngine.Random.Range(-125, 125); } while (Mathf.Abs(rota) < 20f);
+                    transform.Rotate(new Vector3(0, rota ,0));
+                    Debug.Log("tomare");
+                    time = 0;
+                }
+                
                  
             }
         }
